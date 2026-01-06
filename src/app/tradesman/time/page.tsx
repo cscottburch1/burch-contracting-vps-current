9fa7f6b664cd07dae89ecfb63c56ab5f9599227c
cscottburch1 <scott.burchcontracting@gmail.com>
2026-01-05 22:14:04 -0500

    Fix useRouter import in time page

--- src/app/tradesman/time/page.tsx
+++ src/app/tradesman/time/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter, useParams } from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function TradesmanTimeTracking() {
   const router = useRouter();
